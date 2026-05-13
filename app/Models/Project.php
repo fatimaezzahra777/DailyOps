2b4ff65ee26f9108f2c65eb87b6e8e94fbfb7002
lakhouil maryem <lkhwilmaryem@gmail.com>
2026-05-13 14:56:19 +0100

    definir les champs autorisée dans fillable

---
+++ app/Models/Project.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'status',
+        'assigned_to',
+        'start_date',
+        'end_date',
+    ];
+
+    protected $casts = [
+        'start_date' => 'date',
+        'end_date' => 'date',
+    ];
+}
