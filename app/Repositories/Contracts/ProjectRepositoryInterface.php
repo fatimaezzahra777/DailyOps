36579c137a6b694b3626f6ea6ac68be5ca204b5c
lakhouil maryem <lkhwilmaryem@gmail.com>
2026-05-13 11:07:08 +0100

    implementer les methodes de interfaces ProjectREpositoryInterface

---
+++ app/Repositories/Contracts/ProjectRepositoryInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+interface ProjectRepositoryInterface
+{
+    public function getAll();
+
+    public function findById($id);
+
+    public function store(array $data);
+
+    public function update($id, array $data);
+
+    public function delete($id);
+
+    public function searchAndFilter($request);
+}
