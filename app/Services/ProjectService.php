a2d7686f79809d44f6cfdfc919d09bb9f26887d0
lakhouil maryem <lkhwilmaryem@gmail.com>
2026-05-13 11:20:11 +0100

    le srvice Projetc service done

---
+++ app/Services/ProjectService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Contracts\ProjectRepositoryInterface;
+
+class ProjectService
+{
+    protected $projectRepository;
+
+    public function __construct(ProjectRepositoryInterface $projectRepository)
+    {
+        $this->projectRepository = $projectRepository;
+    }
+
+    public function getAllProjects()
+    {
+        return $this->projectRepository->getAll();
+    }
+
+    public function getProjectById($id)
+    {
+        return $this->projectRepository->findById($id);
+    }
+
+    public function createProject(array $data)
+    {
+        return $this->projectRepository->store($data);
+    }
+
+    public function updateProject($id, array $data)
+    {
+        return $this->projectRepository->update($id, $data);
+    }
+
+    public function deleteProject($id)
+    {
+        return $this->projectRepository->delete($id);
+    }
+
+    public function filterProjects($request)
+    {
+        return $this->projectRepository->searchAndFilter($request);
+    }
+}
