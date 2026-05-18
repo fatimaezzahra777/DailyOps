e1a8e194dd72267020a0a5c55039accb40345c99
lakhouil maryem <lkhwilmaryem@gmail.com>
2026-05-18 15:58:28 +0100

    ajouter le model comments avec les champs autorisé

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\ProjectRepository;
 use App\Repositories\Contracts\ProjectRepositoryInterface;
+use App\Repositories\Contracts\TaskRepositoryInterface;
+use App\Repositories\TaskRepository;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -13,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(ProjectRepositoryInterface::class,ProjectRepository::class);
+        $this->app->bind(TaskRepositoryInterface::class,TaskRepository::class);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\TaskController;
+
 
 Route::get('/', function () {
     return redirect()->route('projects.index');
@@ -22,10 +24,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/projects/calendar', [ProjectController::class, 'calendar'])->name('projects.calendar');
     Route::get('/projects/reports', [ProjectController::class, 'reports'])->name('projects.reports');
     Route::resource('projects', ProjectController::class);
-
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('tasks', TaskController::class);
+
+
 });
 
 
